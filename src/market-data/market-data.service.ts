88027e9c326567d7910b0da821af2c15ceb538f3
yevheniia <y.horshchyk@thein1.com>
2025-06-12 15:05:40 +0200

    feat: Add mongodb

--- src/market-data/market-data.service.ts
+++ src/market-data/market-data.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { BinanceService } from '../providers/binance/binance.service';
 import { GetHistoricalTrades } from './dtos/get-historical-trades.dto';
+import { Trade, TradeSchema } from '../database/schemas/trade.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class MarketDataService {
-  constructor(private readonly binanceService: BinanceService) {}
+  constructor(
+    private readonly binanceService: BinanceService,
+    @InjectModel(Trade.name) private tradeModel: Model<Trade>,
+  ) {}
 
   async getMarketData(data: GetHistoricalTrades) {
     const trades = await this.binanceService.getHistoricalTrades(
@@ -13,6 +19,18 @@ export class MarketDataService {
       data.fromId,
     );
 
-    console.log('trades', trades);
+    const formattedTrades =  trades.map((trade) => {
+      const t = {
+        id: trade.id,
+        price: trade.price,
+        qty: trade.qty,
+        quoteQty: '5614.58583840',
+        time: 1749732692290,
+        isBuyerMaker: true,
+        isBestMatch: true,
+      };
+    })
+
+    //await this.tradeModel.insertMany(trades, { ordered: false });
   }
 }
