c5cddb0236b233240e48baa09172c129ddb955f4
yevheniia <y.horshchyk@thein1.com>
2025-06-12 14:31:26 +0200

    feat: Add http service

--- src/providers/binance/binance.module.ts
+++ src/providers/binance/binance.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BinanceService } from './binance.service';
+import { HttpModule } from '@nestjs/axios';
+
+const BINANCE_API = 'https://api.binance.com';
 
 @Module({
+  imports: [HttpModule.register({ baseURL: BINANCE_API })],
   providers: [BinanceService],
   exports: [BinanceService],
 })
--- src/providers/binance/binance.service.ts
+++ src/providers/binance/binance.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
-
+import { HttpService } from '@nestjs/axios';
 @Injectable()
-export class BinanceService {}
+export class BinanceService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async getHistoricalTrades(symbol: string) {
+    const trades = await this.httpService.axiosRef.get(
+      `/api/v3/historicalTrades?symbol=${symbol}`,
+    );
+
+    return trades;
+  }
+}
