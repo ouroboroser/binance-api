6f9bd49c9b549755993fe732c2b8f1573edf30c8
yevheniia <y.horshchyk@thein1.com>
2025-06-12 14:24:29 +0200

    feat: Add market data contorller

--- src/providers/binance/binance.module.ts
+++ src/providers/binance/binance.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { BinanceService } from './binance.service';
-import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [HttpModule.register()],
   providers: [BinanceService],
   exports: [BinanceService],
 })
